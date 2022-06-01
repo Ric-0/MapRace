bccbf7b453b102d0723507788faa391aa72b11ca
Ric-0 <nicolas.prevost566@gmail.com>
2022-06-01 19:42:30 +0200

    Clean debut

--- components/Map.js
+++ components/Map.js
@@ -1,45 +1,9 @@
-
-import React, { useState, useEffect, TouchableHighlight, Text } from "react";
-import { StyleSheet, View, PermissionsAndroid } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, PermissionsAndroid, TouchableOpacity, Modal, Text, Button, Animated, Polyline, Pressable } from 'react-native';
 import MapView from 'react-native-maps';
 import { Dimensions } from 'react-native';
 import { Marker } from "react-native-maps";
-import {decode} from "@mapbox/polyline"; //please install this package before running!
-const getDirections = async (startLoc, destinationLoc) => {
-  try {
-    const KEY = "sylvan-faculty-345110"; //put your API key here.
-    //otherwise, you'll have an 'unauthorized' error.
-    let resp = await fetch(
-      `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=${KEY}`
-    );
-    let respJson = await resp.json();
-    let points = decode(respJson.routes[0].overview_polyline.points);
-    console.log(points);
-    let coords = points.map((point, index) => {
-      return {
-        latitude: point[0],
-        longitude: point[1]
-      };
-    });
-    return coords;
-  } catch (error) {
-    return error;
-  }
-};
-const options = {
-  container: {
-    backgroundColor: '#FF0000',
-    padding: 5,
-    borderRadius: 5,
-    width: 200,
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 25,
-    color: '#FFF',
-    marginLeft: 7,
-  },
-};
+
 const mapStyle = [
   {
     "elementType": "geometry",
@@ -259,119 +223,82 @@ const mapStyle = [
   }
 ]; //map styles go here!
 
-export default function app() {
-  const [isTimerStart, setIsTimerStart] = useState(false);
-  const [isStopwatchStart, setIsStopwatchStart] = useState(false);
-  const [timerDuration, setTimerDuration] = useState(90000);
-  const [resetTimer, setResetTimer] = useState(false);
-  const [resetStopwatch, setResetStopwatch] = useState(false);
-  const [coords, setCoords] = useState([]);
 
-  useEffect(() => {
-    //fetch the coordinates and then store its value into the coords Hook.
-    getDirections("52.5200066,13.404954", "50.1109221,8.6821267")
-      .then(coords => setCoords(coords))
-      .catch(err => console.log("Something went wrong"));
-  }, []);
+const ModalPoup = ({visible, children}) => {
+  const [showModal, setShowModal] = React.useState(visible);
+  const scaleValue = React.useRef(new Animated.Value(0)).current;
+  React.useEffect(() => {
+    toggleModal();
+  }, [visible]);
+  const toggleModal = () => {
+    if (visible) {
+      setShowModal(true);
+      Animated.spring(scaleValue, {
+        toValue: 1,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    } else {
+      setTimeout(() => setShowModal(false), 200);
+      Animated.timing(scaleValue, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: true,
+      }).start();
+    }
+  };
+  return (
+    <Modal transparent visible={showModal}>
+      <View style={styles.modalBackGround}>
+        <Animated.View
+          style={[styles.modalContainer, {transform: [{scale: scaleValue}]}]}>
+          {children}
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+export default function app() {
+  const [coords, setCoords] = useState([]);
+  const [visible, setVisible] = React.useState(false);
 
   return (
     <View style={styles.container}>
-      <SafeAreaView style={styles.container}>
-      <View style={styles.container}>
-        <Text style={styles.title}>
-          Example of React Native Timer and Stopwatch
-        </Text>
-        <View style={styles.sectionStyle}>
-          <Stopwatch
-            laps
-            msecs
-            start={isStopwatchStart}
-            // To start
-            reset={resetStopwatch}
-            // To reset
-            options={options}
-            // Options for the styling
-            getTime={(time) => {
-              console.log(time);
-            }}
-          />
-          <TouchableHighlight
-            onPress={() => {
-              setIsStopwatchStart(!isStopwatchStart);
-              setResetStopwatch(false);
-            }}>
-            <Text style={styles.buttonText}>
-              {!isStopwatchStart ? 'START' : 'STOP'}
-            </Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={() => {
-              setIsStopwatchStart(false);
-              setResetStopwatch(true);
-            }}>
-            <Text style={styles.buttonText}>RESET</Text>
-          </TouchableHighlight>
-        </View>
-        <View style={styles.sectionStyle}>
-          <Timer
-            totalDuration={timerDuration}
-            msecs
-            // Time Duration
-            start={isTimerStart}
-            // To start
-            reset={resetTimer}
-            // To reset
-            options={options}
-            // Options for the styling
-            handleFinish={() => {
-              alert('Custom Completion Function');
-            }}
-            // Can call a function On finish of the time
-            getTime={(time) => {
-              console.log(time);
-            }}
-          />
-          <TouchableHighlight
-            onPress={() => {
-              setIsTimerStart(!isTimerStart);
-              setResetTimer(false);
-            }}>
-            <Text style={styles.buttonText}>
-              {!isTimerStart ? 'START' : 'STOP'}
-            </Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={() => {
-              setIsTimerStart(false);
-              setResetTimer(true);
-            }}>
-            <Text style={styles.buttonText}>RESET</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    </SafeAreaView>
-      <MapView style={styles.map}
-      
-      initialRegion={{
-        latitude: 47.6667,
-        longitude: -2.75,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-      customMapStyle={mapStyle}>
-      
-
-      {/*marker to a nearby location */}
-      <Marker style={styles.image}
-        coordinate={{
-          latitude: 47.63577,
-          longitude: -2.763085,
+      <MapView 
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
         }}
-        pinColor="green"
       />
-      {/* finally, render the Polyline component with the coords data */}
-      {coords.length > 0 && <Polyline coordinates={coords} />}
-      </MapView>
+      {/*}
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'bottom', position:'absolute', zIndex: 999}}>
+      <ModalPoup visible={visible}>
+        <View style={{alignItems: 'center'}}>
+          <View style={styles.header}>
+            <TouchableOpacity onPress={() => setVisible(false)}>
+              <Text>X</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        <Text style={{marginVertical: 30, fontSize: 20, textAlign: 'center'}}>
+          Course nÂ°1
+        </Text>
+        <Button title="Lancer la course"/>
+      </ModalPoup>
+      <Pressable style={styles.button} onPress={() => setVisible(true)}>
+        <Text style={styles.text}>Voir la course</Text>
+      </Pressable>
+    </View>
+
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'bottom', position:'absolute', zIndex: -10}}>
+          <Text>Zoomez pour afficher les courses</Text>
+        </View>
+  {*/}
+
     </View>
   );
 }
@@ -387,8 +314,50 @@ const styles = StyleSheet.create({
       width: Dimensions.get('window').width,
       height: Dimensions.get('window').height,
     },
-    image: {
-      flex: 1,
-      width: 5,
-      height: 5, }
+    modalBackGround: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'bottom',
+    justifyContent: 'bottom',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 10,
+    elevation: 3,
+    backgroundColor: 'green'
+    },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'black',
+  },
+  btnVoirCourse: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
+    color: '#FFC0CB',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+  },
+  modalContainer: {
+    width: '80%',
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+    borderRadius: 20,
+    elevation: 20,
+  },
+  header: {
+    width: '100%',
+    height: 40,
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+  }
   });
