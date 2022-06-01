7a3b6ca2b4bbcc7671c36d7bc7bbf2da338cab83
Ric-0 <nicolas.prevost566@gmail.com>
2022-06-01 18:17:38 +0200

    Debut

---
+++ components/Map.js
@@ -0,0 +1,394 @@
+
+import React, { useState, useEffect, TouchableHighlight, Text } from "react";
+import { StyleSheet, View, PermissionsAndroid } from 'react-native';
+import MapView from 'react-native-maps';
+import { Dimensions } from 'react-native';
+import { Marker } from "react-native-maps";
+import {decode} from "@mapbox/polyline"; //please install this package before running!
+const getDirections = async (startLoc, destinationLoc) => {
+  try {
+    const KEY = "sylvan-faculty-345110"; //put your API key here.
+    //otherwise, you'll have an 'unauthorized' error.
+    let resp = await fetch(
+      `https://maps.googleapis.com/maps/api/directions/json?origin=${startLoc}&destination=${destinationLoc}&key=${KEY}`
+    );
+    let respJson = await resp.json();
+    let points = decode(respJson.routes[0].overview_polyline.points);
+    console.log(points);
+    let coords = points.map((point, index) => {
+      return {
+        latitude: point[0],
+        longitude: point[1]
+      };
+    });
+    return coords;
+  } catch (error) {
+    return error;
+  }
+};
+const options = {
+  container: {
+    backgroundColor: '#FF0000',
+    padding: 5,
+    borderRadius: 5,
+    width: 200,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 25,
+    color: '#FFF',
+    marginLeft: 7,
+  },
+};
+const mapStyle = [
+  {
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "weight": 1
+      }
+    ]
+  },
+  {
+    "elementType": "labels.icon",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#212121"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.country",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#9e9e9e"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.land_parcel",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "administrative.locality",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#bdbdbd"
+      }
+    ]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#181818"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text.stroke",
+    "stylers": [
+      {
+        "color": "#1b1b1b"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#c8cc0d"
+      },
+      {
+        "weight": 2
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [
+      {
+        "color": "#c8cc0d"
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#8a8a8a"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#373737"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#3c3c3c"
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway.controlled_access",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#4e4e4e"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#616161"
+      }
+    ]
+  },
+  {
+    "featureType": "transit",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#757575"
+      }
+    ]
+  },
+  {
+    "featureType": "transit.line",
+    "elementType": "geometry.fill",
+    "stylers": [
+      {
+        "color": "#c8cc0d"
+      },
+      {
+        "weight": 1
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#000000"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "labels.text.fill",
+    "stylers": [
+      {
+        "color": "#3d3d3d"
+      }
+    ]
+  }
+]; //map styles go here!
+
+export default function app() {
+  const [isTimerStart, setIsTimerStart] = useState(false);
+  const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+  const [timerDuration, setTimerDuration] = useState(90000);
+  const [resetTimer, setResetTimer] = useState(false);
+  const [resetStopwatch, setResetStopwatch] = useState(false);
+  const [coords, setCoords] = useState([]);
+
+  useEffect(() => {
+    //fetch the coordinates and then store its value into the coords Hook.
+    getDirections("52.5200066,13.404954", "50.1109221,8.6821267")
+      .then(coords => setCoords(coords))
+      .catch(err => console.log("Something went wrong"));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Example of React Native Timer and Stopwatch
+        </Text>
+        <View style={styles.sectionStyle}>
+          <Stopwatch
+            laps
+            msecs
+            start={isStopwatchStart}
+            // To start
+            reset={resetStopwatch}
+            // To reset
+            options={options}
+            // Options for the styling
+            getTime={(time) => {
+              console.log(time);
+            }}
+          />
+          <TouchableHighlight
+            onPress={() => {
+              setIsStopwatchStart(!isStopwatchStart);
+              setResetStopwatch(false);
+            }}>
+            <Text style={styles.buttonText}>
+              {!isStopwatchStart ? 'START' : 'STOP'}
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              setIsStopwatchStart(false);
+              setResetStopwatch(true);
+            }}>
+            <Text style={styles.buttonText}>RESET</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={styles.sectionStyle}>
+          <Timer
+            totalDuration={timerDuration}
+            msecs
+            // Time Duration
+            start={isTimerStart}
+            // To start
+            reset={resetTimer}
+            // To reset
+            options={options}
+            // Options for the styling
+            handleFinish={() => {
+              alert('Custom Completion Function');
+            }}
+            // Can call a function On finish of the time
+            getTime={(time) => {
+              console.log(time);
+            }}
+          />
+          <TouchableHighlight
+            onPress={() => {
+              setIsTimerStart(!isTimerStart);
+              setResetTimer(false);
+            }}>
+            <Text style={styles.buttonText}>
+              {!isTimerStart ? 'START' : 'STOP'}
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight
+            onPress={() => {
+              setIsTimerStart(false);
+              setResetTimer(true);
+            }}>
+            <Text style={styles.buttonText}>RESET</Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    </SafeAreaView>
+      <MapView style={styles.map}
+      
+      initialRegion={{
+        latitude: 47.6667,
+        longitude: -2.75,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+      customMapStyle={mapStyle}>
+      
+
+      {/*marker to a nearby location */}
+      <Marker style={styles.image}
+        coordinate={{
+          latitude: 47.63577,
+          longitude: -2.763085,
+        }}
+        pinColor="green"
+      />
+      {/* finally, render the Polyline component with the coords data */}
+      {coords.length > 0 && <Polyline coordinates={coords} />}
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      width: Dimensions.get('window').width,
+      height: Dimensions.get('window').height,
+    },
+    image: {
+      flex: 1,
+      width: 5,
+      height: 5, }
+  });
